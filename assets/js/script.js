01c6469e45aeac86602fbfdbd004e9e5b6328ede
adilsanak <adilsaputra0101@gmail.com>
2021-02-27 19:01:42 +0700

    navbar

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// Show menu
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId);
+
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('show-menu')
+        })
+    }
+}
+showMenu('nav-toggle', 'nav-menu');
